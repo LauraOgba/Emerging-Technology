9ab6f58694eaa27a466f0b9403dedd537622020a
LauraOgba <lauraelohoogba4@gmail.com>
2018-11-30 02:36:33 +0000

    Mnist Dataset

--- digitrec.py
+++ digitrec.py
@@ -14,30 +14,86 @@
 #                                                                                                #
 #=============================================================================================== #
 
-# Using Keras to train the data from tensorflow
-import tensorflow as tf
-from tensorflow import keras
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from keras.layers.normalization import BatchNormalization
 
-# Using the help of other libraries such as Numpy and Matplotlib
-import numpy as np
-import matplotlib.pyplot as plt
+batch_size = 128
+num_classes = 10
+epochs = 10
 
-print(tf.__version__)
+# input image dimensions
+img_rows, img_cols = 28, 28
 
-# Import the handwritten dataset I wrote using paint
-fashion_mnist = keras.datasets.fashion_mnist
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-#Handwritten_Data = keras.datasets.Handwritten_Data
+if K.image_data_format() == 'DataRecognition':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
 
-# Then traing these images
-(train_images, train_labels), (test_images, test_labels) = Handwritten Data.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 
-class_names = ['Number0', 'Number1', 'Number2', 'Number3', 'Number4', 
-               'Number5', 'Number6', 'Number7', 'Number8', 'Number9']
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
-train_images.shape
-len(train_labels)
+model = Sequential()
 
-train_labels
-test_images.shape
-len(test_labels)
+model.add(Conv2D(32, (3, 3), input_shape=(28,28,1)))
+model.add(Activation('relu'))
+BatchNormalization(axis=-1)
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+BatchNormalization(axis=-1)
+model.add(Conv2D(64,(3, 3)))
+model.add(Activation('relu'))
+BatchNormalization(axis=-1)
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+# Fully connected layer
+
+BatchNormalization()
+model.add(Dense(512))
+model.add(Activation('relu'))
+BatchNormalization()
+model.add(Dropout(0.2))
+model.add(Dense(10))
+
+model.add(Activation('softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=2,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=1)
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+model.save('mnist_keras_cnn_model.h5')
